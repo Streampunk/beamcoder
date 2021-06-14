4817866bf40f31fb91c31895a65b9d1780410919
Simon Rogers <scriptorian@streampunk.media>
2021-06-14 16:07:00 +0100

    Remove redundant setter leading to channel layout warning message

--- src/filter.cc
+++ src/filter.cc
@@ -992,13 +992,7 @@ void filtererExecute(napi_env env, void* data) {
       }
       p = c->outParams[i].find("channel_layouts");
       if (p != c->outParams[i].end()) {
-        const int64_t out_channel_layout = av_get_channel_layout(p->second.c_str());
-        int out_channel_counts[] = { av_get_channel_layout_nb_channels(out_channel_layout), -1 };
-        ret = av_opt_set_int_list(sinkCtx, "channel_counts", out_channel_counts, -1,
-                                  AV_OPT_SEARCH_CHILDREN);
-        if (ret < 0) { av_log(NULL, AV_LOG_ERROR, "Cannot set output channel count\n"); }
-
-        const int64_t out_channel_layouts[] = { out_channel_layout, -1 };
+        const int64_t out_channel_layouts[] = { (int64_t)av_get_channel_layout(p->second.c_str()), -1 };
         ret = av_opt_set_int_list(sinkCtx, "channel_layouts", out_channel_layouts, -1,
                                   AV_OPT_SEARCH_CHILDREN);
         if (ret < 0) { av_log(NULL, AV_LOG_ERROR, "Cannot set output channel layout\n"); }
